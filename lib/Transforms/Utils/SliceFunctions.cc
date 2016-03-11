eff69f473486e2d1391099b600f560deb4c6430c
jorge <navasjorgea@gmail.com>
2016-03-10 20:47:58 -0800

    Added log messages.

--- lib/Transforms/Utils/SliceFunctions.cc
+++ lib/Transforms/Utils/SliceFunctions.cc
@@ -1,7 +1,7 @@
 /* Slice the program on to a set of selected functions by command line */
 
 #include "llvm/ADT/SCCIterator.h"
-#include "llvm/Analysis/CallGraph.h"
+//#include "llvm/Analysis/CallGraph.h"
 #include "llvm/IR/CallSite.h"
 #include "llvm/IR/InstIterator.h"
 #include "llvm/IR/InstVisitor.h"
@@ -9,14 +9,15 @@
 #include "llvm/Pass.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
+#include "avy/AvyDebug.h"
 
 #include <set>
 #include <vector>
 
 static llvm::cl::list<std::string>
-FunctionNames("slice-function",
-              llvm::cl::desc("Slice program onto this function"),
-              llvm::cl::ZeroOrMore);
+FuncNamesToKeep("slice-function",
+                llvm::cl::desc("Slice program onto these functions"),
+                llvm::cl::ZeroOrMore);
 
 namespace seahorn {
 
@@ -32,29 +33,37 @@ namespace seahorn {
     SliceFunctions (): ModulePass (ID) {}
     
     virtual bool runOnModule (Module &M) {
-      if (FunctionNames.begin() == FunctionNames.end())
+
+      if (M.empty ()) 
+        return false;
+
+      if (FuncNamesToKeep.begin() == FuncNamesToKeep.end()) 
         return false;
       
-      std::set<Function *> funcs;
-      for (std::string fname : FunctionNames) {
-        Function *F = M.getFunction(fname);
-        if (!F) {
-          errs() << "Warning: " << fname
-                 << " not found. No functions will be removed.\n";
-          printFunctionsInfo(M);
-          return false;
+      std::set<Function *> FuncsToKeep;
+      for (std::string FuncName : FuncNamesToKeep) {
+        Function *Func = M.getFunction(FuncName);
+        if (!Func) {
+          LOG ("slice",  
+               errs() << "SliceFunctions: " << FuncName <<  " not found.\n");
+        } else {
+          FuncsToKeep.insert(Func);
         }
-        funcs.insert(F);
       }
-      
+
+      // -- assume that we slice at least onto one function otherwise
+      //    the module will be empty.
+      if (FuncsToKeep.empty ())
+        return false;
+
       bool Change = false;
-      // Delete function bodies and set external linkage
+
+      // --- Delete function bodies and set external linkage
       for (Function &F : M) {
-        if (F.isDeclaration())
-          continue;
-        if (funcs.count(&F) > 0)
-          continue;
-        
+        if (F.isDeclaration()) continue;
+        if (FuncsToKeep.count(&F) > 0) continue;
+        LOG ("slice", 
+             errs () << "SliceFunctions: deleted body of " << F.getName () <<"\n");
         F.deleteBody();
         Change = true;
       }
@@ -70,7 +79,8 @@ namespace seahorn {
       
       for (GlobalAlias *GA : aliases) {
         if (Function *aliasee = dyn_cast<Function>(GA->getAliasee())) {
-          if (!(funcs.count(aliasee) > 0)) {
+          if (!(FuncsToKeep.count(aliasee) > 0)) {
+            LOG ("slice", errs () << "SliceFunctions: removed alias " << *GA << "\n");
             GA->replaceAllUsesWith(aliasee);
             M.getAliasList().erase(GA);
             Change = true;
@@ -78,14 +88,15 @@ namespace seahorn {
         }
       }
       
-      // Remove main if user says so. This means that DummyMain
-      // function must be run later otherwise the whole module will be
-      // empty.
-      Function *main = M.getFunction("main");
-      if (main && !(funcs.count(main) > 0)) {
-        main->dropAllReferences();
-        main->eraseFromParent();
-        Change = true;
+      // If main is removed then CreateDummyMain pass must be run
+      // otherwise the whole module will be empty.
+      if (Function *Main = M.getFunction("main")) {
+        if (FuncsToKeep.count (Main) <= 0) {
+          LOG ("slice", errs () << "SliceFunctions: deleted main.\n");
+          Main->dropAllReferences();
+          Main->eraseFromParent();
+          Change = true;
+        }
       }
       
       return Change;
@@ -93,7 +104,7 @@ namespace seahorn {
     
     virtual void getAnalysisUsage (AnalysisUsage &AU){
       AU.setPreservesAll();
-      AU.addRequired<llvm::CallGraphWrapperPass>();
+      //AU.addRequired<llvm::CallGraphWrapperPass>();
     }
 
     virtual const char* getPassName () const override 
@@ -101,70 +112,70 @@ namespace seahorn {
 
    private:
 
-    void printFunctionsInfo (Module& M) {
-      CallGraphWrapperPass *cgwp = getAnalysisIfAvailable<CallGraphWrapperPass>();
-      CallGraph *CG = cgwp ? &cgwp->getCallGraph() : nullptr;
+    // void printFunctionsInfo (Module& M) {
+    //   CallGraphWrapperPass *cgwp = getAnalysisIfAvailable<CallGraphWrapperPass>();
+    //   CallGraph *CG = cgwp ? &cgwp->getCallGraph() : nullptr;
 
-      if (CG) {
-        // errs () << "==== Recursive functions ==== \n";
-        // bool has_rec_func = false;
-        // for (auto it = scc_begin (CG); !it.isAtEnd (); ++it) {
-        //   auto &scc = *it;
-        //   if (std::distance (scc.begin (), scc.end ()) > 1) {
-        //     has_rec_func = true;
-        //     errs () << "SCC={";
-        //     for (CallGraphNode *cgn : scc) {
-        //       if (cgn->getFunction ())
-        //         errs () << cgn->getFunction ()->getName () << ";";
-        //     }
-        //   }
-        // }
-        // if (!has_rec_func) errs () << "None\n";
+    //   if (CG) {
+    //     // errs () << "==== Recursive functions ==== \n";
+    //     // bool has_rec_func = false;
+    //     // for (auto it = scc_begin (CG); !it.isAtEnd (); ++it) {
+    //     //   auto &scc = *it;
+    //     //   if (std::distance (scc.begin (), scc.end ()) > 1) {
+    //     //     has_rec_func = true;
+    //     //     errs () << "SCC={";
+    //     //     for (CallGraphNode *cgn : scc) {
+    //     //       if (cgn->getFunction ())
+    //     //         errs () << cgn->getFunction ()->getName () << ";";
+    //     //     }
+    //     //   }
+    //     // }
+    //     // if (!has_rec_func) errs () << "None\n";
         
-        typedef std::pair<Function *, std::pair<unsigned, unsigned>> func_ty;
-        std::vector<func_ty> funcs;
-        errs() << "=== Call graph and function information === \n";
-        errs() << "TOTAL NUM of FUNCTIONS=" << std::distance(M.begin(), M.end())
-               << "\n";
-        for (auto it = scc_begin(CG); !it.isAtEnd(); ++it) {
-          auto &scc = *it;
-          for (CallGraphNode *cgn : scc) {
-            if (cgn->getFunction() && !cgn->getFunction()->isDeclaration()) {
-              funcs.push_back(std::make_pair(
-                  cgn->getFunction(),
-                  std::make_pair(cgn->getNumReferences(),
-                                 std::distance(cgn->begin(), cgn->end()))));
-            }
-          }
-        }
+    //     typedef std::pair<Function *, std::pair<unsigned, unsigned>> func_ty;
+    //     std::vector<func_ty> funcs;
+    //     errs() << "=== Call graph and function information === \n";
+    //     errs() << "TOTAL NUM of FUNCTIONS=" << std::distance(M.begin(), M.end())
+    //            << "\n";
+    //     for (auto it = scc_begin(CG); !it.isAtEnd(); ++it) {
+    //       auto &scc = *it;
+    //       for (CallGraphNode *cgn : scc) {
+    //         if (cgn->getFunction() && !cgn->getFunction()->isDeclaration()) {
+    //           funcs.push_back(std::make_pair(
+    //               cgn->getFunction(),
+    //               std::make_pair(cgn->getNumReferences(),
+    //                              std::distance(cgn->begin(), cgn->end()))));
+    //         }
+    //       }
+    //     }
         
-        std::sort(funcs.begin(), funcs.end(), [](func_ty p1, func_ty p2) {
-            return (p1.second.first + p1.second.second) >
-                (p2.second.first + p2.second.second);
-          });
+    //     std::sort(funcs.begin(), funcs.end(), [](func_ty p1, func_ty p2) {
+    //         return (p1.second.first + p1.second.second) >
+    //             (p2.second.first + p2.second.second);
+    //       });
         
-        for (auto &p : funcs) {
-          Function *F = p.first;
-          unsigned numInsts = std::distance(inst_begin(F), inst_end(F));
-          errs() << F->getName() << " --- NUM INST=" << numInsts
-                 << " CALLERS=" << p.second.first << " CALLEES=" << p.second.second
-                 << "\n";
-        }
-      } else {
-        // errs () << "Call graph not found.\n";
-        errs() << "=== Function information === \n";
-        errs() << "TOTAL NUM of FUNCTIONS=" << std::distance(M.begin(), M.end())
-               << "\n";
-        for (auto &F : M) {
-          unsigned numInsts = std::distance(inst_begin(&F), inst_end(&F));
-          errs() << F.getName()
-                 << "--- NUM BLOCKS=" << std::distance(F.begin(), F.end())
-                 << " NUM INST=" << numInsts << "\n";
-        }
-      }
-    }
+    //     for (auto &p : funcs) {
+    //       Function *F = p.first;
+    //       unsigned numInsts = std::distance(inst_begin(F), inst_end(F));
+    //       errs() << F->getName() << " --- NUM INST=" << numInsts
+    //              << " CALLERS=" << p.second.first << " CALLEES=" << p.second.second
+    //              << "\n";
+    //     }
+    //   } else {
+    //     // errs () << "Call graph not found.\n";
+    //     errs() << "=== Function information === \n";
+    //     errs() << "TOTAL NUM of FUNCTIONS=" << std::distance(M.begin(), M.end())
+    //            << "\n";
+    //     for (auto &F : M) {
+    //       unsigned numInsts = std::distance(inst_begin(&F), inst_end(&F));
+    //       errs() << F.getName()
+    //              << "--- NUM BLOCKS=" << std::distance(F.begin(), F.end())
+    //              << " NUM INST=" << numInsts << "\n";
+    //     }
+    //   }
+    // }
+
   };
-
 
   char SliceFunctions::ID = 0;
 
