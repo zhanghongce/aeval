4b0967af44d4b5048cc252deb84295be0d408798
Sam Kaufman <emrysk@gmail.com>
2017-04-18 22:02:34 -0400

    uncoordinated parallelism with Boost MPI

--- include/deep/RndLearner.hpp
+++ include/deep/RndLearner.hpp
@@ -11,44 +11,52 @@ using namespace std;
 using namespace boost;
 namespace ufo
 {
-  
+
+  struct SynthResult {
+    const bool foundInvariants;
+    const bool cancelled;
+    SynthResult(const bool fi, const bool c) :
+      foundInvariants(fi), cancelled(c) {}
+  };
+
   class RndLearner
   {
-    private:
-    
+  private:
+
     ExprFactory &m_efac;
     EZ3 &m_z3;
     SMTUtils u;
     ufo::ZSolver<ufo::EZ3> m_smt_solver;
     ufo::ZSolver<ufo::EZ3> m_smt_safety_solver;
-    
+
     CHCs& ruleManager;
-    vector<Expr> decls;
-    vector<LAfactory> lfs;
+    vector<Expr> decls;          // container only mutated by `initializeDecl`
+    vector<LAfactory> lfs;       // container only mutated by `initializeDecl`
     vector<Expr> curCandidates;
     map<int, int> incomNum;
-    int invNumber;
-    int all;
+    int numOfSMTChecks;
 
-    bool densecode;           // catch various statistics about the code (mostly, frequences) and setup the prob.distribution based on them
-    bool shrink;              // consider only a small subset of int constants and samples from the code
-    bool aggressivepruning;   // aggressive pruning of the search space based on SAT/UNSAT (WARNING: may miss some invariants)
-    
+    const bool densecode;           // catch various statistics about the code (mostly, frequences) and setup the prob.distribution based on them
+    const bool shrink;              // consider only a small subset of int constants and samples from the code
+    const bool aggressivepruning;   // aggressive pruning of the search space based on SAT/UNSAT (WARNING: may miss some invariants)
+
+    inline int invNumber() { return decls.size(); }
+
   public:
-    
+
     RndLearner (ExprFactory &efac, EZ3 &z3, CHCs& r, bool b1, bool b2, bool b3) :
-      m_efac(efac), m_z3(z3), ruleManager(r), m_smt_solver (z3), m_smt_safety_solver(z3), u(efac),
-      invNumber(0), all(0),
-      densecode(b1), shrink(b2), aggressivepruning(b3) {}
-    
+      m_efac(efac), m_z3(z3), ruleManager(r), m_smt_solver (z3),
+      m_smt_safety_solver(z3), u(efac), numOfSMTChecks(0), densecode(b1),
+      shrink(b2), aggressivepruning(b3) {}
+
     bool isTautology (Expr a)     // adjusted for big disjunctions
     {
       if (isOpX<TRUE>(a)) return true;
-      
+
       ExprSet disjs;
       getDisj(a, disjs);
       if (disjs.size() == 1) return false;
-      
+
       map<ExprSet, ExprSet> varComb;
       for (auto &a : disjs)
       {
@@ -56,9 +64,9 @@ namespace ufo
         expr::filter (a, bind::IsConst(), std::inserter (avars, avars.begin ()));
         varComb[avars].insert(mkNeg(a));
       }
-      
+
       m_smt_solver.reset();
-      
+
       bool res = false;
       for (auto &v : varComb )
       {
@@ -70,32 +78,32 @@ namespace ufo
       }
       return res;
     }
-    
+
     bool checkCandidates()
     {
       map<int, int> localNum = incomNum; // for local status
-      
+
       for (auto &hr: ruleManager.chcs)
       {
         if (hr.isQuery) continue;
-        
+
         m_smt_solver.reset();
-        
+
         // pushing body
         m_smt_solver.assertExpr (hr.body);
-        
+
         Expr cand1;
         Expr cand2;
         Expr invApp1;
         Expr invApp2;
         Expr lmApp;
-      
+
         // pushing src relation
         if (!isOpX<TRUE>(hr.srcRelation))
         {
           int ind1 = getVarIndex(hr.srcRelation, decls);
           LAfactory& lf1 = lfs[ind1];
-          
+
           if (localNum[ind1] > 0)
           {
             cand1 = curCandidates[ind1];
@@ -106,7 +114,7 @@ namespace ufo
             }
             m_smt_solver.assertExpr(invApp1);
           }
-          
+
           lmApp = conjoin(lf1.learntExprs, m_efac);
           for (int i = 0; i < hr.srcVars.size(); i++)
           {
@@ -114,21 +122,21 @@ namespace ufo
           }
           m_smt_solver.assertExpr(lmApp);
         }
-        
+
         // pushing dst relation
         int ind2 = getVarIndex(hr.dstRelation, decls);
         cand2 = curCandidates[ind2];
         invApp2 = cand2;
         LAfactory& lf2 = lfs[ind2];
-    
+
         for (int i = 0; i < hr.dstVars.size(); i++)
         {
           invApp2 = replaceAll(invApp2, lf2.getVarE(i), hr.dstVars[i]);
         }
-        
+
         m_smt_solver.assertExpr(mk<NEG>(invApp2));
-        
-        all++;
+
+        numOfSMTChecks++;
         boost::tribool res = m_smt_solver.solve ();
         if (res)    // SAT   == candidate failed
         {
@@ -150,13 +158,13 @@ namespace ufo
       }
       return true;
     }
-    
+
     bool checkSafety()
     {
       for (auto &hr: ruleManager.chcs)
       {
         if (!hr.isQuery) continue;
-        
+
         int ind = getVarIndex(hr.srcRelation, decls);
         Expr invApp = curCandidates[ind];
         LAfactory& lf = lfs[ind];
@@ -165,16 +173,16 @@ namespace ufo
         {
           invApp = replaceAll(invApp, lf.getVarE(i), hr.srcVars[i]);
         }
-        
+
         m_smt_safety_solver.assertExpr(invApp);
-        
-        all++;
+
+        numOfSMTChecks++;
         boost::tribool res = m_smt_safety_solver.solve ();
         if (!res) return true;
       }
       return false;
     }
-    
+
     void setupSafetySolver()
     {
       // setup the safety solver
@@ -187,54 +195,49 @@ namespace ufo
         }
       }
     }
-    
+
     void initializeDecl(Expr invDecl)
     {
       assert (invDecl->arity() > 2);
-      assert(decls.size() == invNumber);
-      assert(lfs.size() == invNumber);
-      assert(curCandidates.size() == invNumber);
-      
+
       decls.push_back(invDecl->arg(0));
       lfs.push_back(LAfactory(m_efac, densecode, aggressivepruning));  // indeces at decls, lfs, and curCandidates should be the same
       curCandidates.push_back(NULL);
-      
+
       LAfactory& lf = lfs.back();
-      
+
       // collect how many rules has invDecl on the right side
       for (auto &hr : ruleManager.chcs)
       {
         if (hr.dstRelation == decls.back())
-        incomNum[invNumber]++;
+        incomNum[invNumber()-1]++;
       }
-      
-      invNumber++;
-      
+
       for (int i = 1; i < invDecl->arity()-1; i++)
       {
         Expr new_name = mkTerm<string> ("__v__" + to_string(i - 1), m_efac);
         Expr var = bind::mkConst(new_name, invDecl->arg(i));
         lf.addVar(var);
       }
-      
+
       set<int> progConsts;
       set<int> intCoefs;
       vector<CodeSampler> css;
-      
+
       // analize each rule separately:
       for (auto &hr : ruleManager.chcs)
       {
         if (hr.dstRelation != decls.back() && hr.srcRelation != decls.back()) continue;
-        
+
         css.push_back(CodeSampler(hr, invDecl, lf.getVars()));
         css.back().analyzeCode(densecode, shrink);
-        
+
         for (auto &cand : css.back().candidates)
         {
           getLinCombCoefs(cand, intCoefs);
           getLinCombConsts(cand, css.back().intConsts);
         }
-        
+
         // convert cs.intConsts to progConsts and add additive inverses (if applicable):
         for (auto &a : css.back().intConsts)
         {
@@ -242,7 +245,7 @@ namespace ufo
           progConsts.insert(-a);
         }
       }
-      
+
       // sort progConsts and push to vector:
       while (progConsts.size() > 0)
       {
@@ -257,7 +260,7 @@ namespace ufo
         progConsts.erase(min);
         lf.addConst(min);
       }
-      
+
       intCoefs.insert(1);                           // add 1
       for (auto &a : lf.getConsts()) if (a != 0) intCoefs.insert(a);
       for (auto &a : intCoefs) intCoefs.insert(-a); // add the inverse
@@ -280,11 +283,11 @@ namespace ufo
           }
         }
       }
-      
+
       if (densecode)
       {
         int multip = PRIORSTEP;                 // will add +PRIORSTEP to every occurrence
-        
+
         // or, alternatively multip can depend on the type of CHC:
         //        if (cs.hr.isFact) multip = 1;
         //        else if (cs.hr.isQuery) multip = 2 * PRIORSTEP;
@@ -293,20 +296,20 @@ namespace ufo
         {
           // specify weights for OR arity
           lf.orAritiesDensity[lcs.arity] += multip;
-          
+
           for (int i = 0; i < lcs.arity; i++)
           {
             LAterm& lc = lcs.dstate[i];
-            
+
             // specify weights for PLUS arity
             lf.plusAritiesDensity[lc.arity] += multip;
-            
+
             // specify weights for const
             lf.intConstDensity[lc.intconst] += multip;
-            
+
             // specify weights for comparison operation
             lf.cmpOpDensity[lc.cmpop] += multip;
-            
+
             // specify weights for var combinations
             set<int> vars;
             int vars_id = -1;
@@ -321,7 +324,7 @@ namespace ufo
             }
             assert(vars_id >= 0);
             lf.varDensity[lc.arity][vars_id] += multip;
-            
+
             for (int j = 1; j < lc.vcs.size(); j = j+2)
             {
               lf.coefDensity[ lc.vcs [j-1] ] [lc.vcs [j] ] += multip;
@@ -333,20 +336,25 @@ namespace ufo
         lf.printCodeStatistics();
       }
     }
-    
-    void synthesize(int maxAttempts)
+
+    SynthResult synthesize(int maxAttempts, function<bool()> shouldStop)
     {
       bool success = false;
       int iter = 1;
-    
+
       std::srand(std::time(0));
-      
+
       for (int i = 0; i < maxAttempts; i++)
       {
+        // bail if we've been cancelled
+        if (shouldStop()) {
+           return SynthResult(false, true);
+        }
+
         // first, guess candidates for each inv.declaration
-        
+
         bool skip = false;
-        for (int j = 0; j < invNumber; j++)
+        for (int j = 0; j < invNumber(); j++)
         {
           if (curCandidates[j] != NULL) continue;   // if the current candidate is good enough
           LAfactory& lf = lfs[j];
@@ -365,14 +373,14 @@ namespace ufo
           }
           curCandidates[j] = cand;
         }
-        
+
         if (skip) continue;
-        
+
         outs() << "\n  ---- new iteration " << iter++ <<  " ----\n";
-        for (int j = 0; j < invNumber; j++) outs () << "candidate for " << *decls[j] << ": " << *curCandidates[j] << "\n";
-        
+        for (int j = 0; j < invNumber(); j++) outs () << "candidate for " << *decls[j] << ": " << *curCandidates[j] << "\n";
+
         // check all the candidates at once for all CHCs :
-        
+
         if (checkCandidates())
         {
           if (checkSafety())       // query is checked here
@@ -381,23 +389,28 @@ namespace ufo
             break;
           }
         }
-            
-        for (int j = 0; j < invNumber; j++)  curCandidates[j] = NULL; // preparing for the next iteration
+
+        for (int j = 0; j < invNumber(); j++)  curCandidates[j] = NULL; // preparing for the next iteration
       }
-      
-      if (success) outs () << "\n -----> Success after " << --iter      << " iterations\n";
-      else         outs () <<      "\nNo success after " << maxAttempts << " iterations\n";
-      
-      outs () << "        total number of SMT checks: " << all << "\n";
+
+      if (success) {
+        outs () << "\n -----> Success after " << --iter << " iterations\n";
+        outs () << "        total number of SMT checks: " << numOfSMTChecks << "\n";
+        return SynthResult(true, false);
+      } else {
+        outs () << "\nNo success after " << maxAttempts << " iterations\n";
+        outs () << "        total number of SMT checks: " << numOfSMTChecks << "\n";
+        return SynthResult(false, false);
+      }
     }
   };
-  
-  
-  inline void learnInvariants(string smt, int maxAttempts, bool b1=true, bool b2=true, bool b3=true)
+
+
+  inline SynthResult learnInvariants(function<bool()> shouldStop, string smt, int maxAttempts, bool b1=true, bool b2=true, bool b3=true)
   {
     ExprFactory m_efac;
     EZ3 z3(m_efac);
-    
+
     CHCs ruleManager(m_efac, z3);
     ruleManager.parse(smt);
     RndLearner ds(m_efac, z3, ruleManager, b1, b2, b3);
@@ -409,14 +422,15 @@ namespace ufo
       outs() << "WARNING: learning multiple invariants is currently unstable\n"
              << "         it is suggested to disable \'aggressivepruning\'\n";
     }
-    
+
     for (auto& dcl: ruleManager.decls)
     {
+      outs() << "initializing decl:" << *dcl << "\n";
       ds.initializeDecl(dcl);
     }
-  
-    ds.synthesize(maxAttempts);
-  };
+
+    return ds.synthesize(maxAttempts, shouldStop);
+  }
 }
 
 #endif
