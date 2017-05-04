6a36e48e157d63d188c56a0db4bf7948bbfff6e2
Sam Kaufman <emrysk@gmail.com>
2017-05-03 17:04:52 -0700

    benchmark.py now records iterations

--- tools/deep/benchmark.py
+++ tools/deep/benchmark.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import os
 import re
 import sys
+import math
 import time
 import json
 import argparse
@@ -10,8 +11,9 @@ import tempfile
 import subprocess
 
 
+SUCCESS_ITERS_RE = re.compile(r'\s*\-+>\s+Success after (.*) iterations\s*')
 ELAPSED_RE = re.compile(r'\s*elapsed: (.*)s\s*')
-PROC_NUMS_TO_TRY = [1, 4]
+PROCS_TO_TRY = [1, 4]
 
 
 class NoSuccessException(Exception):
@@ -31,12 +33,21 @@ def run_deephorn(example_path, proc_cnt, logs_dir_path):
 
 def parse_log_dir_for_time(dirpath):
     """Returns seconds if any log in `path` is successful. Raises otherwise."""
+    iter_cnt, elapsed_time = None, float('Inf')
     for filename in os.listdir(dirpath):
         with open(os.path.join(dirpath, filename), 'r') as f:
             for line in f.readlines()[-5:]:
-                m = ELAPSED_RE.match(line)
-                if m:
-                    return float(m.group(1))
+                elapsed_match = ELAPSED_RE.match(line)
+                iters_match = SUCCESS_ITERS_RE.match(line)
+                if elapsed_match:
+                    elapsed_fnd = float(elapsed_match.group(1))
+                    elapsed_time = min(elapsed_time, elapsed_fnd)
+                if iters_match:
+                    iters_fnd = int(iters_match.group(1))
+                    if iter_cnt is None or iters_fnd < iter_cnt:
+                        iter_cnt = iters_fnd
+    if iter_cnt is not None and not math.isinf(elapsed_time):
+        return iter_cnt, elapsed_time
     raise NoSuccessException("no success in " + dirpath)
 
 
@@ -58,37 +69,44 @@ def main():
     if args.outdir and not os.path.exists(args.outdir):
         os.makedirs(args.outdir)
 
-    times = {s: {num: [] for num in PROC_NUMS_TO_TRY} for s in args.SMTPATHS}
+    times = {s: {num: [] for num in PROCS_TO_TRY} for s in args.SMTPATHS}
+    iter_cnts = {s: {num: [] for num in PROCS_TO_TRY} for s in args.SMTPATHS}
     unsuccess_cnts = {s: 0 for s in args.SMTPATHS}
     try:
         for i in range(args.iters):
             for spath in args.SMTPATHS:
                 tmp_dir = tempfile.mkdtemp()
-                for pcnt in PROC_NUMS_TO_TRY:
+                for pcnt in PROCS_TO_TRY:
                     start = time.time()
                     run_deephorn(spath, pcnt, tmp_dir)
                     try:
-                        t = parse_log_dir_for_time(tmp_dir)
+                        i, t = parse_log_dir_for_time(tmp_dir)
                     except NoSuccessException:
                         t = time.time() - start
                         unsuccess_cnts[spath] += 1
                     times[spath][pcnt].append(t)
+                    iter_cnts[spath][pcnt].append(i)
     except KeyboardInterrupt:
         pass
 
     # Save the times
     if args.outdir:
         with open(os.path.join(args.outdir, "times.json"), 'w') as f:
-            json.dump({'times': times, 'unsuccess_cnts': unsuccess_cnts}, f)
+            json.dump({'times': times,
+                       'iter_cnts': iter_cnts,
+                       'unsuccess_cnts': unsuccess_cnts}, f)
 
     # Print the times
-    for smtpath, subtime in times.iteritems():
+    for smtpath in times.iterkeys():
+        subtime, subiters = times[smtpath], iter_cnts[smtpath]
         asterisk = ":"
         if unsuccess_cnts[smtpath]:
             asterisk = " [unsuccessful " + str(unsuccess_cnts[smtpath]) + "]:"
         print(smtpath + asterisk)
         for pcnt, t in subtime.iteritems():
             print("\t" + str(pcnt) + " process(es) took: " + str(sorted(t)))
+        for pcnt, t in subiters.iteritems():
+            print("\t" + str(pcnt) + " process(es) iterated: " + str(sorted(t)))
 
     # Draw histogram
     if args.hist:
