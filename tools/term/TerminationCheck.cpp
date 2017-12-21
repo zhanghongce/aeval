1adc4b50ea5888334e0c42b6bb758dc34153cc25
grigoryfedyukovich <grigory.fedyukovich@gmail.com>
2017-12-21 18:03:59 -0500

    a (more) user-friendly command-line interface

--- tools/term/TerminationCheck.cpp
+++ tools/term/TerminationCheck.cpp
@@ -3,22 +3,44 @@
 using namespace ufo;
 using namespace std;
 
-bool getBoolValue(const char * opt, bool defValue, int argc, char ** argv)
+bool getBoolValue(const char * opt, bool defValue, int * argc, char ** argv)
 {
-  for (int i = 1; i < argc; i++)
+  for (int i = 1; i < *argc; i++)
   {
-    if (strcmp(argv[i], opt) == 0) return true;
+    if (strcmp(argv[i], opt) == 0)
+    {
+      std::copy(argv + i + 1, argv + *argc, argv + i);
+      (*argc)--;
+      return true;
+    }
   }
   return defValue;
 }
 
-int getIntValue(const char * opt, int defValue, int argc, char ** argv)
+bool getBoolValue(const char * opt, const char * opt2, bool defValue, int * argc, char ** argv)
 {
-  for (int i = 1; i < argc-1; i++)
+  for (int i = 1; i < *argc - 1; i++)
+  {
+    if (strcmp(argv[i], opt) == 0 && strcmp(argv[i+1], opt2) == 0)
+    {
+      std::copy(argv + i + 2, argv + *argc, argv + i);
+      *argc = *argc - 2;
+      return true;
+    }
+  }
+  return defValue;
+}
+
+int getIntValue(const char * opt, int defValue, int * argc, char ** argv)
+{
+  for (int i = 1; i < *argc-1; i++)
   {
     if (strcmp(argv[i], opt) == 0)
     {
-      return atoi(argv[i+1]);
+      int res = atoi(argv[i+1]);
+      std::copy(argv + i + 2, argv + *argc, argv + i);
+      *argc = *argc - 2;
+      return res;
     }
   }
   return defValue;
@@ -26,7 +48,7 @@ int getIntValue(const char * opt, int defValue, int argc, char ** argv)
 
 int main (int argc, char ** argv)
 {
-  if (getBoolValue("--help", false, argc, argv) || argc < 2){
+  if (getBoolValue("--help", false, &argc, argv) || argc < 2){
     outs () <<
     "* * *                         FreqTerm v.0.1 - Copyright (C) 2017                         * * *\n" <<
     "                                      Grigory Fedyukovich                                      \n\n" <<
@@ -47,14 +69,14 @@ int main (int argc, char ** argv)
     return 0;
   }
 
-  int nonterm = getIntValue("--nonterm", 3, argc, argv);
-  int rank = getIntValue("--rank", 3, argc, argv);
-  int mrg = getIntValue("--transform", 0, argc, argv);
-  int lw = getBoolValue("--lightweight", false, argc, argv);
+  int nonterm = getIntValue("--nonterm", 3, &argc, argv);
+  int rank = getIntValue("--rank", 3, &argc, argv);
+  int mrg = getIntValue("--transform", 0, &argc, argv);
+  int lw = getBoolValue("--lightweight", false, &argc, argv);
 
-  int sp = getBoolValue("spacer", false, argc, argv);
-  int fr = getBoolValue("freqhorn", false, argc, argv);
-  int ki = getBoolValue("kind", false, argc, argv);
+  int sp = getBoolValue("--solver", "spacer", false, &argc, argv);
+  int fr = getBoolValue("--solver", "freqhorn", false, &argc, argv);
+  int ki = getBoolValue("--solver", "kind", false, &argc, argv);
 
   if (sp + fr + ki > 1)
   {
@@ -69,9 +91,24 @@ int main (int argc, char ** argv)
   else if (fr > 0) slv = freqhorn;
   else slv = kind;
 
+  if (argc > 2)
+  {
+    outs () << "Could not parse some of the following options: ";
+    for (int i = 1; i < argc; i++) outs () << "\"" << argv[i] << "\", ";
+    outs () << "\n";
+    return 0;
+  }
+
   if (nonterm == 0 && rank == 0)
   {
     outs() << "All fun is skipped\n";
+    return 0;
+  }
+
+  std::ifstream infile(argv[argc-1]);
+  if (!infile.good())
+  {
+    outs() << "Could not read file \"" << argv[argc-1] << "\"\n";
     return 0;
   }
 
